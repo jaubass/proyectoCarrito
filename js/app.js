1e9322b862c4cf701f516bceff1f51c432e0f110
jau <jau-mac@MacBook-Pro-de-jau.local>
2024-02-24 19:30:08 +0100

    Añadir cursos al carrito

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -17,6 +18,43 @@ function cargarEventListeners() {
 function agregarCurso(e) {
     e.preventDefault();
     if( e.target.classList.contains('agregar-carrito') ) {
-        console.log('Its fine!')
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
+}
+
+
+function leerDatosCurso(curso) {
+    console.log(curso)
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+
+        
+    }
+    // console.log(infoCurso)
+    // Agregar datos al carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    console.log(articulosCarrito);
+    carritoHTML();
+}
+
+// Muestra el HTLM al carrito
+
+function carritoHTML() {
+    articulosCarrito.forEach( curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+
+        // Agregar el HTML del carrito en el tbody del index.html
+        contenedorCarrito.appendChild(row);
+    })
 }
